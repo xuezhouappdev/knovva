291f947d43818dbf7e1bcdb920f8a83612f6927e
Xue Zhou <xuezhouapplication@gmail.com>
2016-12-29 22:20:05 -0500

    added animation and scroll function to partner div

---
+++ js/index.js
@@ -0,0 +1,35 @@
+/**
+ * Created by xuezhou on 12/29/16.
+ */
+
+<!-- Script to change the panel for login dialoug-->
+$("#a_pwd").click(function(){
+    $(".login").addClass("hidden",5000);
+    $(".pwforgot").removeClass("hidden");
+});
+
+$("#a_newusr").click(function(){
+    $(".login").addClass("hidden",5000);
+    $(".newuser").removeClass("hidden");
+});
+
+$(".close").click(function(){
+    location.reload();
+});
+
+
+
+<!-- Script to activate the carousel -->
+$('.carousel').carousel({
+    interval: 5000 //changes the speed
+});
+
+
+<!-- Script to control partner items -->
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 3400) {
+            $("#hide-div").delay(100).animate({opacity:'1'},1500);
+        }
+    });
+
+
