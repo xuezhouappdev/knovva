d0ab4849f8e12fbac1fc31e4d66534be81491d5a
Xue Zhou <xuezhouapplication@gmail.com>
2017-01-21 22:35:20 -0500

    separated js from php

--- intro.php
+++ intro.php
@@ -246,72 +246,9 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <script src="bootstrap-3.3.7/js/bootstrap.min.js"></script>
 
-<script>
-
-    // alert("Handler for .click() called.");
+            <script src="js/intro.js"></script>
 
 
-    $(document).ready(function(){
-        var inputnum = $("input").length;
-
-
-
-
-
-        $("#counter").html(1+"/"+inputnum);
-        $(".error-msg").html("");
-        var hash = window.location.hash;
-        hash && $('ul.nav a[href="' + hash + '"]').tab('show');
-
-            //PREV button
-            $(".prev").click(function () {
-                if(parseInt($('li.nav-item.active').find('a').attr('aria-controls'))>1) {
-                    $("#counter").html(parseInt($('li.nav-item.active').find('a').attr('aria-controls')) - 1+"/"+inputnum);
-                }
-                $('.nav-tabs').find('.active').prev('li').find('a').tab('show');
-            });
-
-            //NEXT BUTTON---------------------------------------------------------------------------------------------------------------------------------------
-            $(".next").click(function (e) {
-
-                //alert($(this).parents("form").find("input").val());
-
-                if($(this).parents("form").find("input").val()=="" ) {
-                    $(".error-msg").text("Please fill the field before going to the next step");
-                }
-
-                else{
-                    $(".error-msg").text("");
-                    if(parseInt($('.nav-tabs>.active').find('a').attr('aria-controls'))<4) {
-                        $("#counter").html(parseInt($('li.nav-item.active').find('a').attr('aria-controls')) + 1+"/"+inputnum);
-                    }
-                    $('.nav-tabs>.active').next().find('a').tab('show');
-                }
-
-
-            });
-
-        //input verification
-
-
-        //disable the a
-        $('a.nav-link').click(function (e) {
-            // custom handling here
-            $(".error-msg").text("");
-            var counter2 = $(this).attr("aria-controls");
-           // alert(counter2);
-            $("#counter").html(parseInt(counter2)+"/"+inputnum);
-
-        });
-
-        //jump to index page;
-        $("#cross-btn").click(function(){
-            window.location.href = "index.php";
-        })
-
-    });
-
-</script>
 
 </body>
 </html>
---
+++ js/intro.js
@@ -0,0 +1,62 @@
+/**
+ * Created by xuezhou on 1/21/17.
+ */
+$(document).ready(function(){
+    var inputnum = $("input").length;
+
+
+
+
+
+    $("#counter").html(1+"/"+inputnum);
+    $(".error-msg").html("");
+    var hash = window.location.hash;
+    hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+    //PREV button
+    $(".prev").click(function () {
+        if(parseInt($('li.nav-item.active').find('a').attr('aria-controls'))>1) {
+            $("#counter").html(parseInt($('li.nav-item.active').find('a').attr('aria-controls')) - 1+"/"+inputnum);
+        }
+        $('.nav-tabs').find('.active').prev('li').find('a').tab('show');
+    });
+
+    //NEXT BUTTON---------------------------------------------------------------------------------------------------------------------------------------
+    $(".next").click(function (e) {
+
+        //alert($(this).parents("form").find("input").val());
+
+        if($(this).parents("form").find("input").val()=="" ) {
+            $(".error-msg").text("Please fill the field before going to the next step");
+        }
+
+        else{
+            $(".error-msg").text("");
+            if(parseInt($('.nav-tabs>.active').find('a').attr('aria-controls'))<4) {
+                $("#counter").html(parseInt($('li.nav-item.active').find('a').attr('aria-controls')) + 1+"/"+inputnum);
+            }
+            $('.nav-tabs>.active').next().find('a').tab('show');
+        }
+
+
+    });
+
+    //input verification
+
+
+    //disable the a
+    $('a.nav-link').click(function (e) {
+        // custom handling here
+        $(".error-msg").text("");
+        var counter2 = $(this).attr("aria-controls");
+        // alert(counter2);
+        $("#counter").html(parseInt(counter2)+"/"+inputnum);
+
+    });
+
+    //jump to index page;
+    $("#cross-btn").click(function(){
+        window.location.href = "index.php";
+    })
+
+});
