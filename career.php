1d820bee10bca8b1ee4586b706de393cd3d45942
Xue Zhou <xuezhouapplication@gmail.com>
2017-02-06 23:48:35 -0500

    updated career page

--- career.php
+++ career.php
@@ -176,10 +176,157 @@ include "header.php";
             <div class="panel panel-danger">
                 <div class="panel-heading">MARKETING</div>
                 <div class="panel-body">
-                  <h5>Digital Marketing Specialist – Mandarin Speaker</h5>
-                    <h5>SEO Specialist</h5>
-                    <h5>Marketing Analyst</h5>
+                  <h5 data-toggle="modal" data-target="#myModal1">Digital Marketing Specialist – Mandarin Speaker</h5>
 
+
+
+                    <div class="modal fade" id="myModal1" role="dialog">
+                        <div class="modal-dialog">
+
+                            <!-- Modal content-->
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 class="modal-title">Digital Marketing Specialist – Mandarin Speaker</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <p>We are looking for a marketing professional to work with China Marketing Director, U.S. marketing team and business development team for both countries to develop digital marketing strategy and execution plan to attract students from China.</p>
+                                    <h5>How to Apply</h5>
+                                    <p>Please email us your resume and/or portfolio to career@knovva.com. Use the "position" that you want to apply as your email subject. For example: if you want to apply for "SEO Specialist", your email subject should be "Apply for SEO Specialist". If you believe you're a fit for us, find a position below that matches your skillset, apply today!</p>
+                                    <h5>Job Description</h5>
+                                    <ul>Responsibilities
+                                        <li>Implement optimization strategies including, but not limited to technical audits, on-page optimizations, on-site content expansions, site migrations, competitive analysis, internal linking, and structured data markup.</li>
+                                        <li>Manage organic search engine performance and goal-setting based on site impressions, click-through rates, traffic, and conversions.</li>
+                                        <li>Collaborate with content marketing and blog contributors to create high-quality content around important, relevant terms.</li>
+                                        <li>Audit websites for SEO health, user experience, and conversions; provide explanations and recommendations for how to address issues.</li>
+                                        <li>Correct optimization issues that may arise and help identify areas of improvement to ensure success of marketing programs.</li>
+                                        <li>Manage the strategy and setup of all paid online campaigns, including retargeting, display network, and pay-per-click.</li>
+                                        <li>Take ownership of campaign reporting, while continuously thinking of ways to improve upon reporting methodology.</li>
+                                    </ul>
+
+                                    <ul>Qualifications
+                                        <li>2 or more years of experience working on SEO projects and website optimization</li>
+                                        <li>Proficient written Chinese required.</li>
+                                        <li>In-depth knowledge of the various paid marketing channels and technologies, including paid search, retargeting, social network advertising(Facebook, Twitter, LinkedIn, and more), and content distribution and placement networks.
+                                        </li>
+                                        <li>Proficient in Google Analytics and keyword tools.</li>
+                                        <li>Experience with data-driven SEO analysis and optimization.</li>
+                                        <li>General knowledge of HTML and CSS.</li>
+                                        <li>Intermediate proficiency in analytics and Microsoft Office</li>
+                                        <li>Strong analytical, creative problem solving, project management, and cross-team collaboration skills a must.</li>
+
+
+                                    </ul>
+                                   <!-- <form method="post">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <label for="firstname">First Name*</label>
+                                                    <input type="text" class="form-control" name="fn"  placeholder="First Name">
+
+                                                </div>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <label for="lastname">Last Name*</label>
+                                                    <input type="text" class="form-control" name="ln"  placeholder="Last Name">
+
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <label for="email">Email*</label>
+                                                    <input type="email" class="form-control" name="email" placeholder="Email">
+
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label for="phone">Phone</label>
+                                                    <input type="tel" class="form-control" name="phone"  placeholder="Phone">
+
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label for="resume" >Resume</label>
+                                                    <input type="file" style="margin-top:10px" name="attachment" id="attachment" >
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+
+                                        <div class="row">
+                                            <div class="col-md-12" style="text-align: center">
+                                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                                <button type="submit" class="btn btn-default">Submit</button>
+
+
+                                            </div>
+                                        </div>
+
+                                    </form>-->
+                                    <div style="text-align: center;margin-top: 20px;margin-bottom: 20px;">
+                                    <a href="mailto:career@knovva.com?Subject=Apply for SEO Specialist" target="_top"> Apply Now</a>
+                                    </div>
+
+                                </div>
+                               <!-- <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                </div>-->
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                    <h5 data-toggle="modal" data-target="#myModal2">SEO Specialist</h5>
+                    <div class="modal fade" id="myModal2" role="dialog">
+                        <div class="modal-dialog">
+
+                            <!-- Modal content-->
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 class="modal-title">Modal Header</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Some text in the modal.</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+
+                    <h5 data-toggle="modal" data-target="#myModal3">Marketing Analyst</h5>
+                    <div class="modal fade" id="myModal3" role="dialog">
+                        <div class="modal-dialog">
+
+                            <!-- Modal content-->
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 class="modal-title">Modal Header</h4>
+                                </div>
+                                <div class="modal-body">
+                                    <p>Some text in the modal.</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
 
 
 
@@ -254,7 +401,7 @@ include "header.php";
     })
 
 
-  
+
 
 
 </script>
