1eccdc3afc7641ed68a7e680c22862f31d47411b
Xue Zhou <xuezhouapplication@gmail.com>
2017-04-18 10:32:40 -0400

    updated faq page style

--- js/accordian.js
+++ js/accordian.js
@@ -8,9 +8,9 @@ $(document).ready(function(){
         //alert("TEt");
         $(this).css("text-decoration","none");
         $(".panel-heading").css({"background":"white","color":"black"});
-        $(".panel-heading i").removeClass("fa-minus-circle").addClass("fa-plus-circle").css("color","#ff3e68");
+        $(".panel-heading i").removeClass("fa-minus-circle").addClass("fa-plus-circle").css("color","#6E539E");
 
-        $(this).parent().parent().css({"background":"#ff3e68","color":"white"});
+        $(this).parent().parent().css({"background":"#6E539E","color":"white"});
         $(this).prev().addClass("fa-minus-circle").removeClass("fa-plus-circle").css("color","white");
 
 
