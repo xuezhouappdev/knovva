be496c6c041570bc9508c9ac6aef7c3efa6f2279
Xue Zhou <xuezhouapplication@gmail.com>
2017-04-21 01:20:27 -0400

    updated webinar page

---
+++ jobs.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>  Jobs | Knovva
+    </title>
+
+
+    <!-- Program base CSS-->
+    <link rel="stylesheet" href="css/jobs.css">
+
+
+
+    <!-- Google KeyWord -->
+
+    <meta name="keywords" content="" />
+
+
+
+
+</head>
+
+
+<?php
+// include header
+include "header.php";
+?>
+
+<body>
+
+
+
+
+
+
+<div class="footer-wrapper">
+    <?php
+    include ("footer.php");
+    ?>
+</div>
+
+
+
+
+
+
+
+
+</body>
+</html>
--- webinar.php
+++ webinar.php
@@ -37,69 +37,144 @@
 
 
 
-<!-- Live-->
-<div class="live section">
-    <div class="container">
-     <div class="row">
-         <div class="col-md-8">
-             <iframe class="video" width="100%" height="500" src="https://www.youtube.com/embed/9aaMnIRkK3k$showinfo=0" frameborder="1" allowfullscreen></iframe>
 
-         </div>
-         <div class="col-md-4">
+<!-- banner -->
+<div class="outmost">
+    <div class="banner section" style="opacity: 1">
 
-             <div class="panel panel-default">
-                 <div class="panel-heading">
-                    Past Videos
-                 </div>
-                 <div class="panel-body">
+        <div class="herotext">
+            <h1>Live Webinar</h1>
+            <a id="play">PLAY NOW</a>
 
-                         <p class="video " > <i class="fa fa-youtube-play infinite  bounceIn" aria-hidden="true"></i><a data-fancybox href="https://www.youtube.com/watch?v=b_xCApa88Yg&amp;autoplay=1&amp;rel=0&amp;showinfo=0">
-                             2017 Model G20 Delegate Orientation
-                         <p class="video " > <i class="fa fa-youtube-play infinite  bounceIn" aria-hidden="true"></i><a data-fancybox href="https://www.youtube.com/watch?v=b_xCApa88Yg&amp;autoplay=1&amp;rel=0&amp;&amp;showinfo=0">
-                                 2017 Model G20 Delegate Orientation
-                             </a></p>
-                         <p class="video "> <i class="fa fa-youtube-play infinite  bounceIn" aria-hidden="true"></i><a data-fancybox href="https://www.youtube.com/watch?v=b_xCApa88Yg&amp;autoplay=1&amp;rel=0&amp;&amp;showinfo=0">
-                                 2017 Model G20 Delegate Orientation
-                             </a></p>
-                         <p class="video "> <i class="fa fa-youtube-play infinite  bounceIn" aria-hidden="true"></i><a data-fancybox href="https://www.youtube.com/watch?v=b_xCApa88Yg&amp;autoplay=1&amp;rel=0&amp;&amp;showinfo=0">
-                                 2017 Model G20 Delegate Orientation
-                             </a></p>
-                         <p class="video "> <i class="fa fa-youtube-play infinite  bounceIn" aria-hidden="true"></i><a data-fancybox href="https://www.youtube.com/watch?v=b_xCApa88Yg&amp;autoplay=1&amp;rel=0&amp;&amp;showinfo=0">
-                                 2017 Model G20 Delegate Orientation
-                             </a></p>
-                         <p class="video "> <i class="fa fa-youtube-play infinite bounceIn" aria-hidden="true"></i><a data-fancybox href="https://www.youtube.com/watch?v=b_xCApa88Yg&amp;autoplay=1&amp;rel=0&amp;&amp;showinfo=0">
-                                 2017 Model G20 Delegate Orientation
-                             </a></p>
-
-                 </div>
-             </div>
-
-             <script>
-                 $( document ).ready(function() {
-
-                     $(".video").mouseover(function(){
-                         $(this).find("i").addClass("animated");
-                     });
-
-                     $(".video").mouseout(function(){
-                         $(this).find("i").removeClass("animated");
-                     });
-                 });
+            <!--control the video-->
+            <script>
+                $("#play").click(function(){
 
 
+                    $(".banner.section").animate({
+                        opacity: 0},1000)
+                });
+
+            </script>
+
+        </div>
 
 
-             </script>
-
-
-         </div>
-     </div>
     </div>
+
+    <!-- Live-->
+    <div class="live section" style="position: absolute;top:60px;z-index: -1;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12" style="height: 100vh">
+                    <h5>Now Playing: </h5>
+                    <!--             <iframe class="video" width="100%" height="600" src="https://www.youtube.com/embed/9aaMnIRkK3k$showinfo=0" frameborder="1" allowfullscreen></iframe>-->
+                    <embed height="75%" width="100%" quality="high" allowfullscreen="true" type="application/x-shockwave-flash" src="//static.hdslb.com/miniloader.swf" flashvars="aid=9641357&page=1" pluginspage="//www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash"></embed>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 </div>
 
 
-<!-- History-->
 
+
+
+<hr>
+<!-- History-->
+<div class="history section">
+    <div class="container">
+        <h3>Past Webinars</h3>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="imgwrapper">
+
+                        <img class="img img-responsive" src="image/webinar/dummy.jpg">
+                    </div>
+                    <div class="text">
+
+                        <h5>2017 Model G20 Delegate Orientation</h5>
+                        <p><i class="fa fa-clock-o" aria-hidden="true"></i>&nbsp;Thu July 14, 2016 09:00pm</p>
+                        <a data-fancybox href="video/sample.mp4"><i class="fa fa-play-circle" aria-hidden="true"></i>&nbsp;Watch Playback</a>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="imgwrapper">
+                        <img class="img img-responsive" src="image/webinar/dummy.jpg">
+                    </div>
+                    <div class="text">
+                        <h5>2017 Model G20 Delegate Orientation</h5>
+                        <p><i class="fa fa-clock-o" aria-hidden="true"></i>&nbsp;Thu July 14, 2016 09:00pm</p>
+                        <a data-fancybox href="//static.hdslb.com/miniloader.swf?aid=9641357&page=1"><i class="fa fa-play-circle" aria-hidden="true"></i>&nbsp;Watch Playback</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="imgwrapper">
+                        <img class="img img-responsive" src="image/webinar/dummy.jpg">
+                    </div>
+                    <div class="text">
+                        <h5>2017 Model G20 Delegate Orientation</h5>
+                        <p><i class="fa fa-clock-o" aria-hidden="true"></i>&nbsp;Thu July 14, 2016 09:00pm</p>
+                        <a data-fancybox href="https://www.youtube.com/watch?v=b_xCApa88Yg&amp;autoplay=1&amp;rel=0&amp;&amp;showinfo=0"><i class="fa fa-play-circle" aria-hidden="true"></i>&nbsp;Watch Playback</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="imgwrapper">
+                        <img class="img img-responsive" src="image/webinar/dummy.jpg">
+                    </div>
+                    <div class="text">
+                        <h5>2017 Model G20 Delegate Orientation</h5>
+                        <p><i class="fa fa-clock-o" aria-hidden="true"></i>&nbsp;Thu July 14, 2016 09:00pm</p>
+                        <a data-fancybox href="https://www.youtube.com/watch?v=b_xCApa88Yg&amp;autoplay=1&amp;rel=0&amp;&amp;showinfo=0"><i class="fa fa-play-circle" aria-hidden="true"></i>&nbsp;Watch Playback</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="imgwrapper">
+                        <img class="img img-responsive" src="image/webinar/dummy.jpg">
+                    </div>
+                    <div class="text">
+                        <h5>2017 Model G20 Delegate Orientation</h5>
+                        <p><i class="fa fa-clock-o" aria-hidden="true"></i>&nbsp;Thu July 14, 2016 09:00pm</p>
+                        <a data-fancybox href="https://www.youtube.com/watch?v=b_xCApa88Yg&amp;autoplay=1&amp;rel=0&amp;&amp;showinfo=0"><i class="fa fa-play-circle" aria-hidden="true"></i>&nbsp;Watch Playback</a>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="imgwrapper">
+                    <img class="img img-responsive" src="image/webinar/dummy.jpg">
+                    </div>
+                    <div class="text">
+                        <h5>2017 Model G20 Delegate Orientation</h5>
+                        <p><i class="fa fa-clock-o" aria-hidden="true"></i>&nbsp;Thu July 14, 2016 09:00pm</p>
+                        <a data-fancybox href="https://www.youtube.com/watch?v=b_xCApa88Yg&amp;autoplay=1&amp;rel=0&amp;&amp;showinfo=0"><i class="fa fa-play-circle" aria-hidden="true"></i>&nbsp;Watch Playback</a>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+</div>
 
 
 
