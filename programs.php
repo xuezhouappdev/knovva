591b4ece66eb56f10ef0ee2200216c123475e689
Xue Zhou <xuezhouapplication@gmail.com>
2017-05-04 11:10:11 -0400

    added hotjar code on program page

--- programs.php
+++ programs.php
@@ -279,7 +279,17 @@ include "header.php";
 
 
 
-
+<!-- Hotjar Tracking Code for www.knovva.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:492805,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 
 
 <!--<script>
