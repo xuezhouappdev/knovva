acd9080d13d9747399d0365ecf09c77c7100e4c2
Xue Zhou <xuezhouapplication@gmail.com>
2017-05-25 22:11:22 -0600

    hide career

--- footer.php
+++ footer.php
@@ -40,7 +40,7 @@
                    <li><a href="index">Home</a></li>
                     <li><a href="about">About</a></li>
                     <li><a href="programs">Programs</a></li>
-                    <li><a href="career.php">Career</a></li>
+<!--                    <li><a href="career.php">Career</a></li>-->
 
                    <!-- <li><a href="#">Blog</a></li>-->
                     <li><a href="contact">Contact</a></li>
