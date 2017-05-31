7ac537ae6ce740408f6dbf82a3dd89eb9d447423
Xue Zhou <xuezhouapplication@gmail.com>
2017-05-31 13:21:30 -0400

    updated thank you page

---
+++ thankyou.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>  Thank you for Your interest!
+    </title>
+
+
+
+    <!-- Program -->
+    <link rel="stylesheet" href="css/thankyou.css">
+    <meta name="keywords" content="" />
+
+
+
+</head>
+
+
+<?php
+// include header
+include "header.php";
+?>
+
+<body>
+
+
+ <div class="main">
+     <div class="container">
+         <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
+
+         <h1>Thank you!</h1>
+         <h5>Your submission is received and we will contact you soon.</h5>
+
+         <a href="index.php"> Back to Home</a>
+     </div>
+ </div>
+
+
+
+
+
+
+<div class="footer-wrapper">
+    <?php
+    include ("footer.php");
+    ?>
+</div>
+
+
+
+
+
+</body>
+</html>
