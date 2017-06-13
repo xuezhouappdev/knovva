c12b56cadfc08023dfa4710eeae8f51d00e0fa92
Xue Zhou <xuezhouapplication@gmail.com>
2017-06-13 12:44:51 -0400

    updated promotio code - $3000 level

--- payment.php
+++ payment.php
@@ -66,8 +66,6 @@ include "header.php";
                 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
             </form>
 
-
-
         </div>
 
 
